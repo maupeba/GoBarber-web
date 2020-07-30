3991a9c6a056010a5dd97abf944655029cfa99c5
Mauricio de Sousa <mauriciopeba1234@gmail.com>
2020-07-30 17:49:23 -0300

    configuraçao inicial do context api ---

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationErrros from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -16,6 +17,8 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
